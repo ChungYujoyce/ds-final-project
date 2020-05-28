3a6504b977d16653180e2505d5cffb2ce6542f2c
joyce <joyce.bhps@gmail.com>
2020-05-28 18:18:50 +0800

    add word cloud fountion

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 #from config import DevConfig
 #from sentiment import get_all_words, get_tweets_for_model, remove_noise,process
 #from datetime import timedelta
-#from word_cloud import process_cloud, cloud
+from news_word_cloud import process_cloud
 #import requests
 from news_recommend import news_recommendation
 app = Flask(__name__)
@@ -17,7 +17,12 @@ def about():
 @app.route('/classification')
 def work():
     return render_template('classification.html')
+
 query = ""
+title = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Blue", "Yellow", "Green", "Purple"]
+score = [12, 19, 3, 5, 2, 3, 6, 8, 4, 9]
+fig_name = "cloud_result/wordCloud.png"
+count = 0
 @app.route('/loading', methods=['GET','POST'])
 def load():
     if request.method == 'POST':
@@ -27,25 +32,33 @@ def load():
 
 @app.route('/news_process')
 def process():
-    global query
+    global query, title, score
     print(query)
     data = news_recommendation(query)
-    title = []
-    score = []
     for key, val in data.items():
         title.append(key)
         score.append(val)
-    return render_template('rec.html', score=score, title=title)
+    return render_template('rec.html', fig_name = str(fig_name), score=score, title=title)
     
 @app.route('/news_recommend', methods=['GET','POST'])
 def recommend():
-    title = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Blue", "Yellow", "Green", "Purple"]
-    score = [12, 19, 3, 5, 2, 3, 6, 8, 4, 9]
-    return render_template('rec.html', score=score, title=title)
+    return render_template('rec.html', fig_name = str(fig_name), score=score, title=title)
 
 @app.route('/features')
 def products():
  	return render_template('features.html')
+
+@app.route('/word_cloud', methods=['GET','POST'])
+def word_cloud(): 
+    if request.method == 'POST':
+        global fig_name
+        global count
+        input_text = request.form.get('news')
+        fig_name, font_path, count = process_cloud(input_text, count)
+        print("count: ", count)
+        return render_template('rec.html', fig_name = str(fig_name), score=score, title=title)
+    else:
+        return render_template('rec.html', fig_name = str(fig_name), score=score, title=title)
 
 '''
 @app.route('/sentiment', methods=['GET','POST'])
@@ -71,28 +84,6 @@ def sentiment():
         
     return render_template('sentiment.html')
 
-@app.route('/word_cloud', methods=['GET','POST'])
-def word_cloud(): 
-    if request.method == 'POST':
-        global fig_name
-        input_text = request.form.get('news')
-        input_genre = request.form.get("selectgenre")
-        #picname = request.form.get('genre')
-        fig_name, font_path, back = process_cloud(input_text, str(input_genre))
-        return render_template('word_cloud_result.html', fig_name = str(fig_name), font_path=str(font_path), back=str(back)) 
-    return render_template('word_cloud.html', data=[{'name':'Business'}, {'name':'Education'},{'name':'Entertainment'}, {'name':'Health'},
-                                                    {'name':'Medical'}, {'name':'Sports'}, {'name':'Technology'}, {'name':'Others'}])
-@app.route('/return')
-def back():
- 	return redirect(url_for('index'))
-
-@app.route('/return_cloud')
-def back_1():
- 	return redirect(url_for('word_cloud'))
-
-@app.route('/return_sentiment')
-def back_2():
- 	return redirect(url_for('sentiment'))
 '''
 if __name__ == '__main__':
     app.debug = True
---
+++ news_word_cloud.py
@@ -0,0 +1,58 @@
+import os
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import numpy as np
+from PIL import Image
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+stop_words = list(stopwords.words('english'))
+import random
+
+def process_cloud(text, count):
+    NUMBER_OF_GENRE = 8
+    fonts = [
+        'Gloucester-MT-Extra-Condensed.ttf',
+        'Cooper-Black.ttf',
+        'Harlow-Solid-Italic.ttf',
+        'Broadway.ttf',
+        'Brush-Script-MT-Italic.ttf',
+        'Gill-Sans-Ultra-Bold.ttf',
+        'CopperplateGothicBold.ttf',
+        'Wide-Latin.ttf',
+    ] # genre[i] goes with fonts[i]
+
+    font_path = os.getcwd() + "/static/assets/" + fonts[count]
+    #background_image_path = os.getcwd() + "/assets/" + genres[j] + ".JPG"
+    #background = "assets/" + genres[j] + ".JPG"
+
+    nltk_stopwords = stopwords.words('english')
+    customized_stopwords = ["the","and","i", "'s", "-", "--", "---", "n't", "'in", "``", "`", "'", "''", "'ll", "'ve"]
+    Stopwords = customized_stopwords + nltk_stopwords + list(STOPWORDS)
+
+
+    #input_text = open("./dataset/central/txt_file/elec_22.txt", "r").read()
+    for stop_word in stop_words:
+        regex_stopword = r"\b" + stop_word + r"\b"
+    words = text.replace(regex_stopword, '')
+    words = text.split()
+    #print(words.count('china') , words.count('trump'))
+    if words.count('china') > words.count('trump'):
+        mask = os.getcwd() + "/static/assets/mask.JPG"
+    elif words.count('china') < words.count('trump'):
+        mask = os.getcwd() + "/static/assets/trump.PNG"
+    else:
+        mask = os.getcwd() + "/static/assets/virus.JPG"
+    maskArray = np.array(Image.open(mask))
+        
+    word = WordCloud(stopwords = Stopwords, width = 2000, height = 1500,\
+                     font_path = font_path, min_font_size = 3, max_font_size = 400, max_words = 200,\
+                     background_color = "rgba(255, 255, 255, 0)",  mask = maskArray, \
+                     mode = 'RGBA', colormap = plt.get_cmap('nipy_spectral') # nipy_spectral, tab10
+           ).generate(text)
+    fig_name = "cloud_result/wordCloud_"+str(count)+".png"
+    word.to_file("static/cloud_result/wordCloud_"+str(count)+".png")
+    count += 1
+    if(count == 8):
+        count = 0
+    return fig_name, font_path, count
