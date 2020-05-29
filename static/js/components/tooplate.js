7e880dd0c39f64d73b674eca1e117230661c22df
joyce <joyce.bhps@gmail.com>
2020-05-29 20:07:34 +0800

    add other 2 barcharts

--- static/js/components/tooplate.js
+++ static/js/components/tooplate.js
@@ -62,73 +62,169 @@ function drawLineChart() {
     }
 }
 
-function drawBarChart(data) {
+function drawBarChart(score, title, score2, title2, score3, title3) {
+
+    optionsBar = {
+        responsive: true,
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Similarity Score"
+                    }
+                }
+            ],
+            xAxes: [
+                {
+                    ticks: {
+                        display: false //this will remove only the label
+                    }
+                }]
+        }
+    };
+    optionsBar.maintainAspectRatio =
+        $(window).width() < width_threshold ? false : true;
+
+
+    ctxBar = document.getElementById("barChart").getContext("2d");
+    configBar = {
+        type: "bar",
+        data: {
+            labels: title,//["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "ww", "eee", "www", "sss"],
+            datasets: [
+                {
+                    label: "Score of Similarity",
+                    data: score, //[12, 19, 3, 5, 2, 3, 6, 8, 4, 9],
+                    backgroundColor: [
+                        "rgba(252, 160, 252, 0.2)",
+                        "rgba(249, 133, 133, 0.2)",
+                        "rgba(255, 206, 86, 0.2)",
+                        "rgba(75, 142, 192, 0.2)",
+                        "rgba(255, 99, 132, 0.2)",
+                        "rgba(54, 162, 235, 0.2)",
+                        "rgba(255, 26, 86, 0.2)",
+                        "rgba(75, 192, 192, 0.2)",
+                        "rgba(153, 102, 255, 0.2)",
+                        "rgba(255, 159, 64, 0.2)"
+                    ],
+                    borderColor: [
+                        "rgba(252, 160, 252, 1)",
+                        "rgba(249, 133, 133, 1)",
+                        "rgba(255, 206, 86, 1)",
+                        "rgba(75, 142, 192, 1)",
+                        "rgba(255, 99, 132, 1)",
+                        "rgba(54, 162, 235, 1)",
+                        "rgba(255, 26, 86, 1)",
+                        "rgba(75, 192, 192, 1)",
+                        "rgba(153, 102, 255, 1)",
+                        "rgba(255, 159, 64, 1)"
+                    ],
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: optionsBar
+    };
+
+
+    ctxBar2 = document.getElementById("barChart2").getContext("2d");
+    configBar2 = {
+        type: "bar",
+        data: {
+            labels: title2,//["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "ww", "eee", "www", "sss"],
+            datasets: [
+                {
+                    label: "Score of Similarity",
+                    data: score2, //[12, 19, 3, 5, 2, 3, 6, 8, 4, 9],
+                    backgroundColor: [
+                        "rgba(255, 99, 132, 0.2)",
+                        "rgba(54, 162, 205, 0.2)",
+                        "rgba(255, 226, 86, 0.2)",
+                        "rgba(75, 192, 192, 0.2)",
+                        "rgba(153, 12, 255, 0.2)",
+                        "rgba(57, 162, 235, 0.2)",
+                        "rgba(205, 206, 86, 0.2)",
+                        "rgba(75, 192, 122, 0.2)",
+                        "rgba(153, 102, 25, 0.2)",
+                        "rgba(225, 159, 64, 0.2)"
+                    ],
+                    borderColor: [
+                        "rgba(255, 99, 132, 1)",
+                        "rgba(54, 162, 205, 1)",
+                        "rgba(255, 226, 86, 1)",
+                        "rgba(75, 192, 192, 1)",
+                        "rgba(153, 12, 255, 1)",
+                        "rgba(57, 162, 235, 1)",
+                        "rgba(205, 206, 86, 1)",
+                        "rgba(75, 192, 122, 1)",
+                        "rgba(153, 102, 25, 1)",
+                        "rgba(225, 159, 64, 1)"
+                    ],
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: optionsBar
+    };
+
+    ctxBar3 = document.getElementById("barChart3").getContext("2d");
+    configBar3 = {
+        type: "bar",
+        data: {
+            labels: title3,//["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "ww", "eee", "www", "sss"],//title3,// ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "ww", "eee", "www", "sss"],
+            datasets: [
+                {
+                    label: "Score of Similarity",
+                    data: score3, //[12, 19, 3, 5, 2, 3, 6, 8, 4, 9],
+                    backgroundColor: [
+                        "rgba(114, 248, 11, 0.2)",
+                        "rgba(54, 62, 205, 0.2)",
+                        "rgba(255, 226, 86, 0.2)",
+                        "rgba(75, 102, 192, 0.2)",
+                        "rgba(153, 12, 155, 0.2)",
+                        "rgba(57, 132, 235, 0.2)",
+                        "rgba(205, 206, 86, 0.2)",
+                        "rgba(70, 192, 142, 0.2)",
+                        "rgba(11, 211, 248, 0.2)",
+                        "rgba(248, 11 189, 0.2)"
+                    ],
+                    borderColor: [
+                        "rgba(114, 248, 11, 1)",
+                        "rgba(54, 62, 205, 1)",
+                        "rgba(255, 226, 86, 1)",
+                        "rgba(75, 102, 192, 1)",
+                        "rgba(153, 12, 155, 1)",
+                        "rgba(57, 132, 235, 1)",
+                        "rgba(205, 206, 86, 1)",
+                        "rgba(70, 192, 142, 1)",
+                        "rgba(11, 211, 248, 1)",
+                        "rgba(248, 11 189, 1)"
+                    ],
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: optionsBar
+    };
 
     if ($("#barChart").length) {
-        ctxBar = document.getElementById("barChart").getContext("2d");
-
-        optionsBar = {
-            responsive: true,
-            scales: {
-                yAxes: [
-                    {
-                        ticks: {
-                            beginAtZero: true
-                        },
-                        scaleLabel: {
-                            display: true,
-                            labelString: "Similarity Score"
-                        }
-                    }
-                ]
-            }
-        };
-
-        optionsBar.maintainAspectRatio =
-            $(window).width() < width_threshold ? false : true;
-
-        configBar = {
-            type: "bar",
-            data: {
-                labels: title,// ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "ww", "eee", "www", "sss"],
-                datasets: [
-                    {
-                        label: "# of Similarity",
-                        data: data, //[12, 19, 3, 5, 2, 3, 6, 8, 4, 9],
-                        backgroundColor: [
-                            "rgba(255, 99, 132, 0.2)",
-                            "rgba(54, 162, 235, 0.2)",
-                            "rgba(255, 206, 86, 0.2)",
-                            "rgba(75, 192, 192, 0.2)",
-                            "rgba(153, 102, 255, 0.2)",
-                            "rgba(54, 162, 235, 0.2)",
-                            "rgba(255, 206, 86, 0.2)",
-                            "rgba(75, 192, 192, 0.2)",
-                            "rgba(153, 102, 255, 0.2)",
-                            "rgba(255, 159, 64, 0.2)"
-                        ],
-                        borderColor: [
-                            "rgba(255,99,132,1)",
-                            "rgba(54, 162, 235, 1)",
-                            "rgba(255, 206, 86, 1)",
-                            "rgba(75, 192, 192, 1)",
-                            "rgba(153, 102, 255, 1)",
-                            "rgba(54, 162, 235, 1)",
-                            "rgba(255, 206, 86, 1)",
-                            "rgba(75, 192, 192, 1)",
-                            "rgba(153, 102, 255, 1)",
-                            "rgba(255, 159, 64, 1)"
-                        ],
-                        borderWidth: 1.5
-                    }
-                ]
-            },
-            options: optionsBar
-        };
-
         barChart = new Chart(ctxBar, configBar);
     }
+
+    if ($("#barChart2").length) {
+        barChart2 = new Chart(ctxBar2, configBar2);
+    }
+
+    if ($("#barChart3").length) {
+        barChart3 = new Chart(ctxBar3, configBar3);
+    }
+
 }
+
 
 function drawPieChart() {
     if ($("#pieChart").length) {
@@ -186,9 +282,9 @@ function updateLineChart() {
 }
 
 function updateBarChart() {
-    if (barChart) {
-        barChart.options = optionsBar;
-        barChart.update();
+    if (barChart2) {
+        barChart2.options = optionsBar;
+        barChart2.update();
     }
 }
 
