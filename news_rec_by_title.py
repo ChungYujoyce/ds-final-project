8e15106fd67b1d8c1fe0825083dcd0602cf07e13
joyce <joyce.bhps@gmail.com>
2020-05-29 20:07:56 +0800

    rec model by searching title index

--- news_rec_by_title.py
+++ news_rec_by_title.py
@@ -3,8 +3,7 @@ import requests
 import pandas as pd
 nltk.download('punkt')
 import numpy as np
-import pickle, time
-from datetime import datetime
+import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
@@ -22,12 +21,9 @@ def news_recommendation(item_id, num= 10):
     ids.columns = ['id']
     ds = pd.concat([ds, ids],axis=1)
 
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    start = time.time()
     tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0, stop_words='english')
     tfidf_matrix = tf.fit_transform(ds['Content'])
-    print(tfidf_matrix)
+    #print(tfidf_matrix)
     cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
 
     results = {}
@@ -37,21 +33,93 @@ def news_recommendation(item_id, num= 10):
         similar_items = [(cosine_similarities[idx][i], ds['id'][i]) for i in similar_indices]
 
         results[row['id']] = similar_items[1:]
-        
-    print('done!')
-    # Just reads the results out of the dictionary.
 
-    print("Recommending " + str(num) + " news similar to " + item(item_id, ds) + "...")
-    print("-------")
     recs = results[item_id][:num]
+    titles = []
+    scores = []
     for rec in recs:
-        print("Recommended: " + item(rec[1], ds) + " (score:" + str(rec[0]) + ")")
+        #print("Recommended: " + item(rec[1], ds) + " (score:" + str(rec[0]) + ")")
+        #print(item(rec[1], ds))
+        titles.append(item(rec[1], ds))
+        scores.append(rec[0].item())
+    #print(type(titles[0]), type(scores[1]))
+    return titles, scores
 
-    current_time = now.strftime("%H:%M:%S")
-    print("Finish tfidf processing at", current_time)
-    spent = time.time() - start
-    print("\nTotal spent time: "+str(spent) +"sec\n")
-    return res
+
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+    
+def cosine_recommender(title, top=10):
+    path_df = "Pickles/News_central_rec2.pickle"
+
+    with open(path_df, 'rb') as data:
+        df = pickle.load(data)
+        
+    df['bag_of_words'] = ''
+    columns = df.columns
+    for index, row in df.iterrows():
+        words = ''
+        for col in columns:
+            if col == 'Content':
+                words = words + row[col]+ ' '
+        row['bag_of_words'] = words
+    text = df.bag_of_words.tolist()
+    #title = df.loc[count]['Title']
+    vectorizer = CountVectorizer(text)
+    vectors = vectorizer.fit_transform(text).toarray()
+    df_index = title # set id as user input
+
+    cosines = []
+    for i in range(len(vectors)):
+        vector_list = [vectors[df_index], vectors[i]]
+        cosines.append(cosine_similarity(vector_list)[0,1])
+
+    cosines = pd.Series(cosines)
+    index = cosines.nlargest(top+1).index
+
+    matches = df.loc[index]
+    titles = []
+    scores = []
+    for title,score in zip(matches['Title'][1:],cosines[index][1:]):
+        titles.append(title)
+        scores.append(score)
+
+    return titles, scores
+
+def get_jaccard_sim(str1, str2):
+    a = set(str1.split(','))
+    b = set(str2.split(','))
+    c = a.intersection(b)
+    return(float(len(c)) / (len(a) + len(b) - len(c)))
+
+
+def jaccard_recommender(title, number_of_hits=10):
+    path_df = "Pickles/News_central_rec2.pickle"
+
+    with open(path_df, 'rb') as data:
+        df = pickle.load(data)
+
+    dff = df[title]
+    df['bag_of_words'] = ''
+    columns = df.columns
+    for index, row in df.iterrows():
+        words = ''
+        for col in columns:
+            if col == 'Content':
+                words = words + row[col]+ ' '
+        row['bag_of_words'] = words
+    keyword_string = dff.bag_of_words.iloc[0]
+
+    jaccards = []
+    for news in df['bag_of_words']:
+        jaccards.append(get_jaccard_sim(keyword_string, news))
+    jaccards = pd.Series(jaccards)
+    jaccards_index = jaccards.nlargest(number_of_hits+1).index
+    matches = df.loc[jaccards_index]
+    return zip(matches['Title'][1:],jaccards[jaccards_index][1:]) 
+
+#cosine_recommender(111)
+        
 
 
 
